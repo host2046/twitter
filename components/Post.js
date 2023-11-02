6ffaf57396a491c540de702fcd9c8416df015df1
host2046 <alimizbani888@gmail.com>
2023-11-02 10:04:07 +0330

    add delete functionality to the post updated

--- components/Post.js
+++ components/Post.js
@@ -56,7 +56,9 @@ const Post = ({ post }) => {
   const deletePost = async () => {
     if (window.confirm("Are you sure to delete this post?")) {
       deleteDoc(doc(db, "posts", post.id));
-      deleteObject(ref(storage, `posts/${post.id}/image`));
+      if (post.data().image) {
+        deleteObject(ref(storage, `posts/${post.id}/image`));
+      }
     }
   };
   return (
